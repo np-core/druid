8c980b6507d5cfbeffd92632cb2f838336bfa0f3
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-02-20 16:26:58 +1000

    v0.4.0

--- achilles/terminal/client.py
+++ achilles/terminal/client.py
@@ -2,7 +2,6 @@ import click
 
 from .create import create
 from .train import train
-from .predict import predict
 from .lab import lab
 from .evaluate import evaluate
 
@@ -17,6 +16,5 @@ def terminal_client():
 
 terminal_client.add_command(create)
 terminal_client.add_command(train)
-terminal_client.add_command(predict)
 terminal_client.add_command(lab)
 terminal_client.add_command(evaluate)
