8b7bf0e51bd2eebe4ea4d71975949dd761a41a48
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-02-22 14:44:05 +1000

    v0.4.0

--- achilles/terminal/evaluate/commands.py
+++ achilles/terminal/evaluate/commands.py
@@ -150,12 +150,8 @@ def run_evaluation(model: Path, evaluation: Path, slice: int = None, batch_size:
         predicted_slices = split(
             predicted, [i for i in range(len(predicted)) if i % slice == 0], axis=0
         )
-        for ps in predicted_slices:
-            predicted_probability = product(ps, axis=0)
-            print(predicted_probability)
-        #
-        # predicted_labels = argmax(predicted_probability, 1)
-
+        predicted_probability = [product(ps, axis=0)for ps in predicted_slices]
+        predicted_labels = argmax(predicted_probability, 1)
         exit(0)
     else:
         predicted_labels = argmax(predicted, 1)  # one hot decoded
