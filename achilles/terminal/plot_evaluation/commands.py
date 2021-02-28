55c2b76d0abc3e8ca996e23b8ae715c1a85b54fd
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-03-01 09:49:09 +1000

    v0.4.0

--- achilles/terminal/plot_evaluation/commands.py
+++ achilles/terminal/plot_evaluation/commands.py
@@ -52,7 +52,7 @@ def plot_evaluation(data, plot_file, remove):
             else:
                 rows.append(row)
 
-    df = pandas.DataFrame(rows, columns=df.columns.tolist())
+    df = pandas.DataFrame(rows, columns=[n for n in df.columns.names])
 
     print(f'Average prediction speed: {df.ws.mean():.2f} windows / second')
 
