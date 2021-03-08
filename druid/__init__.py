3bca46358b439d3dd7cb3f8a71cbc3bdaf5a010e
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-03-08 14:56:37 +1000

    v0.1.0

--- druid/__init__.py
+++ druid/__init__.py
@@ -1,4 +1,10 @@
+
+import os
 import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tf.get_logger().setLevel('ERROR')
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
