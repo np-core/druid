05e19594bb7abb2f120e3aa65db16539f16af20f
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-03-22 06:56:03 +1000

    v0.1.0

--- druid/pipeline.py
+++ druid/pipeline.py
@@ -33,7 +33,9 @@ class DruidPipeline:
 
         package_data = {}
         for package, count_data in counts.items():
-            package_data[package] = pandas.DataFrame(count_data).sort_values("name").reset_index().melt()
+            package_data[package] = pandas.DataFrame(count_data).sort_values("name").reset_index().melt(
+                id_vars=['name'], value_vars=['root', 'bacteria', 'archaea']
+            )
             print(package_data[package])
 
         return package_data
