95a45428a39241a7289803299efdaa896f189f2c
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-03-22 07:22:58 +1000

    v0.1.0

--- druid/pipeline.py
+++ druid/pipeline.py
@@ -49,7 +49,7 @@ class DruidPipeline:
 
         fig, axes = plt.subplots(
             nrows=nrow, ncols=ncol, figsize=(
-                nrow * 7, ncol * 4.5
+                nrow * 4.5, ncol * 7
             )
         )
 
