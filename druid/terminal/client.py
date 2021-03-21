d4fefec29dccc487e107e14b72826ef71bcec8c6
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-03-21 11:12:43 +1000

    v0.1.0

--- druid/terminal/client.py
+++ druid/terminal/client.py
@@ -1,9 +1,6 @@
 import click
 
 from .utils import utils
-from .create import create
-from .train import train
-from .evaluate import evaluate
 
 @click.group()
 @click.version_option()
@@ -12,6 +9,3 @@ def terminal_client():
 
 
 terminal_client.add_command(utils)
-terminal_client.add_command(create)
-terminal_client.add_command(train)
-terminal_client.add_command(evaluate)
