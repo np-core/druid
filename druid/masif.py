3746ff6da59b35eb94ff75a36a7bd048b08c1994
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-06-02 15:05:54 +1000

    v0.1.0 masif update

--- druid/masif.py
+++ druid/masif.py
@@ -330,17 +330,20 @@ class Tricorder(PoreLogger):
             try:
                 faces[fi][0] = int(fields[0]) - 1
             except IndexError:
-                faces[fi][0] = None
-
+                # faces[fi][0] = None
+                continue
+                
             try:
                 faces[fi][1] = int(fields[1]) - 1
             except IndexError:
-                faces[fi][1] = None
+                # faces[fi][1] = None
+                continue
 
             try:
                 faces[fi][2] = int(fields[2]) - 1
             except IndexError:
-                faces[fi][2] = None
+                # faces[fi][2] = None
+                continue
 
             count["faces"] -= 1
 
