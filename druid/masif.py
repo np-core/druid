17dbb35dd786366b6d031295a96abfac2543147a
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-06-03 14:43:59 +1000

    v0.1.0 masif update

--- druid/masif.py
+++ druid/masif.py
@@ -14,6 +14,8 @@ import numpy as np
 import pyvista as pv
 from pathlib import Path
 from subprocess import PIPE, Popen
+from numpy.linalg import norm
+from numpy.matlib import repmat
 
 import pymesh
 
@@ -109,11 +111,61 @@ class ProteinModel(PoreLogger):
             self.surface_model['vertices'], self.surface_model['faces']
         )
 
-        print(mesh)
+        regular_mesh = self.regularize_mesh_surface(mesh=mesh, resolution=1.0, detail="normal")
 
-        mesh = self.fix_mesh()
+        # Compute the normals
+        vertex_normal = compute_normal(regular_mesh.vertices, regular_mesh.faces)
 
-        # regular_mesh = fix_mesh(mesh, 1.0)
+    def regularize_mesh_surface(self, mesh, resolution, detail="normal"):
+
+        """ Regularize a protein surface mesh.
+        - based on code from the PyMESH documentation
+        """
+
+        self.logger.info("Regularizing surface mesh using PyMesh")
+
+        bbox_min, bbox_max = mesh.bbox
+        diag_len = norm(bbox_max - bbox_min)
+        if detail == "normal":
+            target_len = diag_len * 5e-3
+        elif detail == "high":
+            target_len = diag_len * 2.5e-3
+        elif detail == "low":
+            target_len = diag_len * 1e-2
+
+        target_len = resolution
+        # print("Target resolution: {} mm".format(target_len));
+        # PGC 2017: Remove duplicated vertices first
+        mesh, _ = pymesh.remove_duplicated_vertices(mesh, 0.001)
+
+        count = 0
+        self.logger.info("Removing degenerated triangles in ten iterations")
+        mesh, __ = pymesh.remove_degenerated_triangles(mesh, 100)
+        mesh, __ = pymesh.split_long_edges(mesh, target_len)
+        num_vertices = mesh.num_vertices
+        while True:
+            mesh, __ = pymesh.collapse_short_edges(mesh, 1e-6)
+            mesh, __ = pymesh.collapse_short_edges(mesh, target_len, preserve_feature=True)
+            mesh, __ = pymesh.remove_obtuse_triangles(mesh, 150.0, 100)
+            if mesh.num_vertices == num_vertices:
+                break
+
+            num_vertices = mesh.num_vertices
+            self.logger.info(f"Iteration {count}: {num_vertices}")
+            count += 1
+            if count > 10:
+                break
+
+        self.logger.info("Resolve inconsistencies in mesh structure")
+        mesh = pymesh.resolve_self_intersection(mesh)
+        mesh, __ = pymesh.remove_duplicated_faces(mesh)
+        mesh = pymesh.compute_outer_hull(mesh)
+        mesh, __ = pymesh.remove_duplicated_faces(mesh)
+        mesh, __ = pymesh.remove_obtuse_triangles(mesh, 179.0, 5)
+        mesh, __ = pymesh.remove_isolated_vertices(mesh)
+        mesh, _ = pymesh.remove_duplicated_vertices(mesh, 0.001)
+
+        return mesh
 
     @staticmethod
     def visualize_surface_mesh(vertices, faces):
@@ -578,6 +630,7 @@ class Tricorder(PoreLogger):
         dev2 = np.pi - abs(dih)
         return min(dev1, dev2)
 
+
 # Exclude disordered atoms.
 class NotDisordered(Select):
     def accept_atom(self, atom):
@@ -738,3 +791,69 @@ AcceptorAngleAtom["OG1"] = "CB"
 # THR donor:
 # Angle: OG1-HG1-X: 180
 DonorAtom["HG1"] = "OG1"
+
+# Normal triangulation
+eps = 1.0e-6
+
+
+def compute_normal(vertex, face):
+    """ Compute the normals of a closed shape.
+
+    Pablo Gainza - LPDI STI EPFL 2019
+    This file is part of MaSIF, based on previous matlab code by Gabriel Peyre, converted to Python by Pablo Gainza
+    compute_normal - compute the normal of a triangulation
+    vertex: 3xn matrix of vertices
+    face: 3xm matrix of face indices.
+
+      normal,normalf = compute_normal(vertex,face)
+
+      normal(i,:) is the normal at vertex i.
+      normalf(j,:) is the normal at face j.
+
+    Copyright (c) 2004 Gabriel Peyr
+    Converted to Python by Pablo Gainza LPDI EPFL 2017  
+    """
+
+    vertex = vertex.T
+    face = face.T
+    nface = np.size(face, 1)
+    nvert = np.size(vertex, 1)
+    normal = np.zeros((3, nvert))
+    # unit normals to the faces
+    normalf = crossp(
+        vertex[:, face[1, :]] - vertex[:, face[0, :]],
+        vertex[:, face[2, :]] - vertex[:, face[0, :]],
+    )
+    sum_squares = np.sum(normalf ** 2, 0)
+    d = np.sqrt(sum_squares)
+    d[d < eps] = 1
+    normalf = normalf / repmat(d, 3, 1)
+    # unit normal to the vertex
+    normal = np.zeros((3, nvert))
+    for i in np.arange(0, nface):
+        f = face[:, i]
+        for j in np.arange(3):
+            normal[:, f[j]] = normal[:, f[j]] + normalf[:, i]
+
+    # normalize
+    d = np.sqrt(np.sum(normal ** 2, 0))
+    d[d < eps] = 1
+    normal = normal / repmat(d, 3, 1)
+    # enforce that the normal are outward
+    vertex_means = np.mean(vertex, 0)
+    v = vertex - repmat(vertex_means, 3, 1)
+    s = np.sum(np.multiply(v, normal), 1)
+    if np.sum(s > 0) < np.sum(s < 0):
+        # flip
+        normal = -normal
+        normalf = -normalf
+    return normal.T
+
+
+def crossp(x, y):
+    # x and y are (m,3) dimensional
+    z = np.zeros((x.shape))
+    z[0, :] = np.multiply(x[1, :], y[2, :]) - np.multiply(x[2, :], y[1, :])
+    z[1, :] = np.multiply(x[2, :], y[0, :]) - np.multiply(x[0, :], y[2, :])
+    z[2, :] = np.multiply(x[0, :], y[1, :]) - np.multiply(x[1, :], y[0, :])
+    return z
